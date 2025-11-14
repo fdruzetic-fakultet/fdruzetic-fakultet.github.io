971e381c2123190b8272bf90cca912f042295040
fdruzetic-fakultet <fdruzetic@student.unipu.hr>
2025-11-14 19:54:03 +0100

    Update DonationBox.sol
    Donation box 2.0 sa svime potrebnim

--- DonationBox.sol
+++ DonationBox.sol
@@ -4,44 +4,56 @@ pragma solidity ^0.8.0;
 
 contract DonationBox{
     address public owner;
-    uint public goal = 100;
+    uint public goal;
     uint256 public deadline;
     uint public totalRaised;
     bool public GoalReached;
     bool public fundsWithdrawn;
+    
 
     event DonationReceived (address donot, uint amout);
     event Withdraw  (address donot, uint amout);
     
 
-    constructor( uint goal, uint _deadline){  
+    constructor( uint _goal, uint256 _durationMinutes){  
         owner = msg.sender;
-        goal = 100000;
-        deadline =  2 days;
+        goal = _goal;
+        deadline = block.timestamp + _durationMinutes;
     }
+
 
     modifier onlyOwner (){
         require(msg.sender == owner, "you are not the owner");
         _;
     }
-    function getTmeLeft(uint secondsPassed) public view returns (bool) {
-        uint256 timeLeft = block.timestamp;
-        return timeLeft >= deadline + secondsPassed;
 
+      
+    function getTimeLeft() public view returns (uint256) {
+        if (block.timestamp >= deadline) return 0;
+        return deadline - block.timestamp;
     }
+
 
     function donate() public payable {
         require(msg.value > 0 , "value cant be empty");
         totalRaised += msg.value;
+        if ( totalRaised >= goal  ){
+            GoalReached = true;
+        }
         emit DonationReceived(msg.sender, msg.value);
+
     }
 
-    function withdraw() public { 
+
+    function withdrawFunds() public { 
+        require(block.timestamp >= deadline || totalRaised >= goal, "time is up");
         uint amount = address(this).balance;
         payable(owner).transfer(amount);
-    emit Withdraw(owner,amount);
+        emit Withdraw(owner,amount);
 
     }
+
+    
 
     function getBalance () public view returns (uint){
         return address(this).balance;
